403c09fb7b91242f06acb5e09aeb14c5dbababee
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2015-12-27 21:39:11 +0800

    debug tools by zyy

---
+++ gen_test/bubble-sort.c
@@ -0,0 +1,33 @@
+#include"trap.h"
+
+#define N 10
+int main() {
+    int a[N];
+    int i = 0;
+    while(i < N) {
+        a[i] = 2233 - (i << 2);
+        i = i + 1;
+    }
+
+    int t;
+    int j=0;
+    while(j < N) {
+        i = 0;
+        while(i < N - 1) {
+            if(a[i] > a[i + 1]) {
+                t = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = t;
+            }
+            i = i + 1;
+        }
+        j = j + 1;
+    }
+
+
+    for (i = 0; i < N-1; i++) {
+        if (a[i] >= a[i + 1]) bad();
+    }
+    good();
+    return 0;
+}
---
+++ gen_test/bubble-sort.d
@@ -0,0 +1,3 @@
+bubble-sort.o: bubble-sort.c \
+ /home/zyy/mips_tool_chain/buildroot-2015.08.1/output/host/usr/mips-buildroot-linux-gnu/sysroot/usr/include/stdc-predef.h \
+ trap.h
