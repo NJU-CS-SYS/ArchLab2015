772287636e16396f7aa8d89272a36beaa4c5772f
Wonicon <conciselove@outlook.com>
2015-12-28 10:24:33 +0800

    Avoid good and bad being optimized

--- ram_init_gen/trap.h
+++ ram_init_gen/trap.h
@@ -1,8 +1,8 @@
 #ifndef TRAP_H
 #define TRAP_H
 
-void good();
-void bad();
+static __attribute__((noinline)) void good() { while (1); }
+static __attribute__((noinline)) void bad() { while (1); }
 
 #define ASSERT(x) do { if (!(x)) bad(); } while (0)
 
