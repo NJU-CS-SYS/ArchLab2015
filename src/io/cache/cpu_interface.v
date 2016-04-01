d271c88b771805a2e9666eb185ecd558b638a24a
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-04-01 16:40:00 +0800

    synthesized cup_interface without warning
    NEXT: testbench

--- src/io/cache/cpu_interface.v
+++ src/io/cache/cpu_interface.v
@@ -25,7 +25,7 @@ module cpu_interface(
     input dmem_read_in, 
     input dmem_write_in,
     input [29:0] dmem_addr,
-    input [31:0] data_reg,
+    input [31:0] data_from_reg,
     input [3:0] dc_byte_w_en,
     input clk,
     input rst,
@@ -36,18 +36,19 @@ module cpu_interface(
 localparam TIMER_PORT   = 32'h00004000;
 localparam KBD_PORT     = 32'h00004010;
 
-wire [31:0] data_from_ram;
+wire [255:0] block_from_ram;
 wire ram_rdy;
 wire ram_en;
 wire ram_write;
 wire [29:0] ram_addr;
+wire [255:0] block_from_dc_to_ram;
+
 reg dc_read_in, dc_write_in;
-wire [31:0] data_from_dc_to_ram;
 
 always @ (*) begin
     dc_read_in = dmem_read_in;
     dc_write_in = dmem_write_in;
-
+/*
     if(dmem_addr == TIMER_PORT[31:2]) begin
         dc_read_in = 0;
         dc_write_in = 0;
@@ -56,38 +57,43 @@ always @ (*) begin
         dc_read_in = 0;
         dc_write_in = 0;
     end
+    */
 end
 
 cache_manage_unit u_cm_0(
-    ic_addr,
-    data_from_ram,
+    rst,
     dc_read_in,
     dc_write_in,
-    dmem_addr,
-    data_reg,
     dc_byte_w_en,
+    ic_addr,
+    dmem_addr,
+    data_from_reg,
+
     clk,
-    rst,
     ram_rdy,
-    ic_data_out,
-    data_from_dc_to_ram,
+    block_from_ram,
+
     mem_stall,
+    dmem_data_out,
+    ic_data_out,
+
     ram_en,
     ram_write,
-    ram_addr
+    ram_addr,
+    block_from_dc_to_ram
 );
 
 ram_top u_ram_0(
-    ram_addr[13:0],
-    data_from_dc_to_ram,
-    clk,
+    rst,
     ram_en,
     ram_write,
-    rst,
+    ram_addr[13:3],
+    block_from_dc_to_ram,
+
+    clk,
     ram_rdy,
-    data_from_ram
+    block_from_ram
 );
 
-assign dmem_data_out = data_from_dc_to_ram;
 
 endmodule
--- src/io/cache/ram_top.v
+++ src/io/cache/ram_top.v
@@ -24,15 +24,23 @@ module ram_top(
     input rst,
     input ram_en,
     input ram_write,
-    input [13:0] ram_addr,
+    input [10:0] ram_addr,
     input [255:0] data_to_ram,
 
-    output clk,
+    input clk,
     output ram_rdy,
-    output reg [255:0] block_out
+    output [255:0] block_out
 );
+
+reg ready;
+reg last_is_disen;
+reg [31:0] count;
+reg [255:0] buffer;
+reg [31:0] data_to_bram;
+wire [31:0] data_from_bram;
+
 xilinx_single_port_ram_no_change u_bram_0(
-    .addra({{ram_addr[13:3]}, {count[2:0]}} ),
+    .addra({{ram_addr[10:0]}, {count[2:0]}} ),
     .dina(data_to_bram),
     .clka(clk),
     .wea(ram_write),
@@ -42,11 +50,33 @@ xilinx_single_port_ram_no_change u_bram_0(
     .douta(data_from_bram)
 );
 
-reg ready;
-reg last_is_disen;
-reg [31:0] count;
-wire [31:0] data_to_bram = data_to_ram[((count + 1) << 5) - 1 : (count<<5)];
-reg [255:0] buffer;
+
+always @(*) begin
+    if(count == 32'd0) begin
+        data_to_bram = data_to_ram[(0 + 1)*32 - 1 : 32*0];
+    end
+    else if(count == 32'd1) begin
+        data_to_bram = data_to_ram[(1 + 1)*32 - 1 : 32*1];
+    end
+    else if(count == 32'd2) begin
+        data_to_bram = data_to_ram[(2 + 1)*32 - 1 : 32*2];
+    end
+    else if(count == 32'd3) begin
+        data_to_bram = data_to_ram[(3 + 1)*32 - 1 : 32*3];
+    end
+    else if(count == 32'd4) begin
+        data_to_bram = data_to_ram[(4 + 1)*32 - 1 : 32*4];
+    end
+    else if(count == 32'd5) begin
+        data_to_bram = data_to_ram[(5 + 1)*32 - 1 : 32*5];
+    end
+    else if(count == 32'd6) begin
+        data_to_bram = data_to_ram[(6 + 1)*32 - 1 : 32*6];
+    end
+    else begin
+        data_to_bram = data_to_ram[(7 + 1)*32 - 1 : 32*7];
+    end
+end
 
 always @ (posedge clk) begin
     if(rst) begin
@@ -64,7 +94,30 @@ always @ (posedge clk) begin
             last_is_disen <= 0;
             if(~last_is_disen) begin
                 count <= count + 1;
-                buffer[((count + 1) << 5) - 1 : (count<<5)] <= data_from_bram;
+                if(count == 32'd0) begin
+                    buffer[(0 + 1)*32 - 1 : 32*0] = data_from_bram;
+                end
+                else if(count == 32'd1) begin
+                    buffer[(1 + 1)*32 - 1 : 32*1] = data_from_bram;
+                end
+                else if(count == 32'd2) begin
+                    buffer[(2 + 1)*32 - 1 : 32*2] = data_from_bram;
+                end
+                else if(count == 32'd3) begin
+                    buffer[(3 + 1)*32 - 1 : 32*3] = data_from_bram;
+                end
+                else if(count == 32'd4) begin
+                    buffer[(4 + 1)*32 - 1 : 32*4] = data_from_bram;
+                end
+                else if(count == 32'd5) begin
+                    buffer[(5 + 1)*32 - 1 : 32*5] = data_from_bram;
+                end
+                else if(count == 32'd6) begin
+                    buffer[(6 + 1)*32 - 1 : 32*6] = data_from_bram;
+                end
+                else begin
+                    buffer[(7 + 1)*32 - 1 : 32*7] = data_from_bram;
+                end
             end
             if(~last_is_disen && count == 32'd8) begin
                 ready = ~ready;
@@ -73,5 +126,6 @@ always @ (posedge clk) begin
     end
 end
 assign ram_rdy = ready;
+assign block_out = buffer;
 
 endmodule
