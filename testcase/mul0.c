385e234958169f32518045697130a29525f76e2c
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-04-28 11:04:48 +0800

    ..

--- testcase/mul0.c
+++ testcase/mul0.c
@@ -13,10 +13,21 @@ int main()
     int a = 9;
     int b = 10;
     int c = 90;
-    if(c == a*b) {
-        return goodtrap();
-    }
-    else {
+    if(c != a*b) {
         return badtrap();
     }
+
+    a = 81;
+    b = 1;
+    c = 81;
+    if(c != a*b) {
+        return badtrap();
+    }
+    a = 7;
+    b = 8;
+    c = 56;
+    if(c != a*b) {
+        return badtrap();
+    }
+    return 0;
 }
