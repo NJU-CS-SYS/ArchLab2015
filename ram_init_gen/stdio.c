c019414982d4ba71d97404499d4b0abb0225c1dd
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-05-21 09:43:53 +0800

    fix bug.....
    ..

--- ram_init_gen/stdio.c
+++ ram_init_gen/stdio.c
@@ -3,7 +3,7 @@ void put_hex(unsigned int x, char *addr) {
     *addr++ = '0';
     *addr++ = 'x';
     for (i = 0; i < 8; i++) {
-        unsigned int val = (x >> ((7 - i) << 2)) & 0xff;
+        unsigned int val = (x >> ((7 - i) << 2)) & 0xf;
         if (val >= 16) {
             *addr++ = '?';
         }
@@ -11,7 +11,7 @@ void put_hex(unsigned int x, char *addr) {
             *addr++ = ((char) val) + '0';
         }
         else {
-            *addr++ = ((char) val) + 'a';
+            *addr++ = ((char) val) - 10 + 'a';
         }
     }
 }
