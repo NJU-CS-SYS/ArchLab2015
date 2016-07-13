e8de321054dcd9d891a82634251f5d9defa201b8
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-07-13 11:32:50 +0800

    ddr read twice

--- testcase/loader.c
+++ testcase/loader.c
@@ -11,16 +11,23 @@
 
 char* vga = VMEM + screen_width*2 + 5;
 
+// #define start_addr 0xf0000000
+#define start_addr 0
+
 int main() {
-  unsigned int addr = 0xf0000100;
+  unsigned int addr = 0x100 | start_addr;
   unsigned int flash_addr = 0xb0000000;
   deref(addr) = deref(flash_addr);
-  for(; addr < 0xf0008000; addr += 4) {
+  for(; addr < (0x8000 | start_addr); addr += 4) {
     deref(addr) = deref(flash_addr);
     flash_addr += 4;
     *vga++ = '.';
   }
 
+#if start_addr
   asm volatile("li $ra, 0xf0000100; jr $ra");
+#else
+  asm volatile("li $ra, 0x100; jr $ra");
+#endif
   return 0;
 }
